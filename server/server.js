c9e31bcf6327f2d125b743477010f43ae05ed9f7
ashik71 <a.ashrafe71@gmail.com>
2019-07-19 21:07:21 +0600

    commit

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'))
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
@@ -442,7 +444,13 @@ app.post('/api/site/site_data',auth,(req, res) => {
     )
 });
 
-
+//DEFAULT
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.port || 3001;
 
