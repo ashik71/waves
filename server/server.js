217b2d2f4571aa56f2dd5b624a73671ca5ef491f
ashik71 <a.ashrafe71@gmail.com>
2019-07-22 22:53:06 +0600

    test deploy p json

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const async = require('async');
 const path = require('path');
 require('dotenv').config();
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
